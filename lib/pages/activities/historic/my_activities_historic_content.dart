0a4687b3cab397a9c6a9c24bde788127904c27c2
Judson <judson.henrique01@gmail.com>
2019-11-04 19:08:41 -0300

    Activities Done page

--- lib/pages/activities/historic/my_activities_historic_content.dart
+++ lib/pages/activities/historic/my_activities_historic_content.dart
@@ -13,13 +13,104 @@ class _DrawerWidgetState extends State<MyActivitiesHistoricContentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Atividades Realizadas"),
-        leading: GestureDetector(onTap: widget.onTap, child: Icon(Icons.menu)),
+      backgroundColor: Color(0xff75dde8),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(85),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(bottomRight: Radius.circular(50), bottomLeft: Radius.circular(50)),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 10,
+                color: Colors.black26,
+                spreadRadius: 5
+              )
+            ]
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: IconButton(
+                  icon: Icon(Icons.menu, size: 30, color: Colors.grey),
+                  onPressed: widget.onTap,
+                ),
+              ),
+              Text("Atividades Realizadas", style: TextStyle(color: Colors.grey, fontSize: 24, fontWeight: FontWeight.bold))
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        child: Text("Atividades Realizadas - Mostrar Histórico das Atividades"),
-      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: listDataTest.length,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (ctx, index){
+            // return Text(listDataTest[index].title);
+            return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(0),
+                  topRight: Radius.circular(0),
+                  bottomLeft: Radius.circular(8),
+                  topLeft: Radius.circular(8),
+                )
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(0), 
+                        topRight: Radius.circular(0), 
+                        bottomLeft: Radius.circular(8), 
+                        topLeft: Radius.circular(8)),
+                      color: listDataTest[index].color,
+                    ),
+                    height: 120,
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(listDataTest[index].title, style: TextStyle(fontSize: 24)),
+                          Divider(),
+                          Text("Tempo restante: " + listDataTest[index].timeLeft, style: TextStyle(fontSize: 16, color: Colors.grey, fontWeight: FontWeight.bold)),
+                          Text("Peso: " + listDataTest[index].weight.toString() + "/Nota: " + listDataTest[index].evaluation.toString(), style: TextStyle(fontSize: 16, color: Colors.grey, fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            );
+          },
+        ),
+      )
     );
   }
+}
+
+List<DataTest> listDataTest = [
+  DataTest(title: "RP1 - Marco I", timeLeft: "3 dias 4 horas", weight: 10, evaluation: 10, color: Colors.green),
+  DataTest(title: "Banco de Dados - Atividade I", timeLeft: "2 dias 1 hora", weight: 10, evaluation: 9, color: Colors.red),
+  DataTest(title: "Mobile- Entrega Protótipo", timeLeft: "8 horas", weight: 10, evaluation: 10, color: Colors.yellow),
+  DataTest(title: "Mobile- Entrega Diagramas", timeLeft: "8 horas", weight: 10, evaluation: 10, color: Colors.grey),
+];
+
+class DataTest {
+  final String title;
+  final String timeLeft;
+  final int weight;
+  final double evaluation;
+  final Color color;
+
+  DataTest({this.title, this.timeLeft, this.weight, this.evaluation, this.color});
 }
